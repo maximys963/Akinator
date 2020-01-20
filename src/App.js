f3f0936a61d9439fc2807e2b585803e6bea1e626
Max Panasenko <maxim.p.jsdev@gmail.com>
2020-01-20 20:24:31 +0200

    add react-router, lint, antd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainScreen from './components/MainScreen/MainScreen';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route
+          path="/"
+          component={MainScreen}
+          exect
+        />
+      </Switch>
     </div>
-  );
-}
+  </Router>
+);
 
 export default App;
---
+++ src/components/MainScreen/MainScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function MainScreen(props) {
+    return (
+        <div>Hello World</div>
+    );
+}
+
+MainScreen.propTypes = {
+
+};
+
+export default MainScreen;
