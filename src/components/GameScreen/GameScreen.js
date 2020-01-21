5385ac6001c7bbe10bcecf1827834cb39d36a0c7
Max Panasenko <maxim.p.jsdev@gmail.com>
2020-01-21 22:25:22 +0200

    get data from audd

--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -1,9 +1,11 @@
 /* eslint-disable */
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button } from 'antd';
+import { config} from "../../etc/config";
 import styles from './GameScreen.module.css';
 
+const { audDApiToken } = config;
 
 function GameScreen() {
   const { TextArea } = Input;
@@ -11,9 +13,35 @@ function GameScreen() {
   const [userName, setUserName] = useState('');
 
   useEffect(() => {
-      const name = localStorage.getItem('gameUserName')
+      const name = localStorage.getItem('gameUserName');
       setUserName(name)
   }, []);
+
+  const onLyricsChange = useCallback( (e) => {
+      const text = e.target.value;
+      setLyrics(text)
+  }, []);
+
+  const onSubmitLyrics = useCallback(async () => {
+      try{
+          const data = JSON.stringify({
+              q: lyrics,
+              api_token: audDApiToken
+          });
+
+          const response = await fetch('https://api.audd.io/findLyrics/?jsonp=?', {
+              method: 'POST',
+              body: data
+          });
+
+          const respTest = await response.text();
+          const cutResponse = JSON.parse(respTest.slice(2, respTest.length -1));
+          console.log('cutResponse');
+          console.log(cutResponse);
+      } catch (err) {
+          console.error(err)
+      }
+  }, [lyrics]);
 
   return (
     <div className={styles.gameContainer}>
@@ -22,10 +50,13 @@ function GameScreen() {
               {`Hello ${userName}, let's play `}
           </div>
           <div className={styles.textArea}>
-        <TextArea />
+        <TextArea
+            onChange={(e) => onLyricsChange(e)}
+        />
           </div>
           <Button
               type="primary"
+              onClick={onSubmitLyrics}
           >
               Guess
           </Button>
