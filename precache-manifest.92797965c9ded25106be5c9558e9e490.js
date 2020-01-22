e09bf0485c4822a9be44d396671166184604fe30
Aaron Green <amor201075@hotmail.com>
2020-01-22 20:46:46 +0200

    Updates

---
+++ precache-manifest.92797965c9ded25106be5c9558e9e490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8782c92605edabe91a08f93e4e45246a",
+    "url": "/Akinator_Int20h/index.html"
+  },
+  {
+    "revision": "e4e4d1eba361397b1f61",
+    "url": "/Akinator_Int20h/static/css/2.e9609898.chunk.css"
+  },
+  {
+    "revision": "3b9c1daca25aa59933f7",
+    "url": "/Akinator_Int20h/static/css/main.807d65ff.chunk.css"
+  },
+  {
+    "revision": "e4e4d1eba361397b1f61",
+    "url": "/Akinator_Int20h/static/js/2.3153456b.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/Akinator_Int20h/static/js/2.3153456b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b9c1daca25aa59933f7",
+    "url": "/Akinator_Int20h/static/js/main.e8d6dbf5.chunk.js"
+  },
+  {
+    "revision": "81004c8abf475025aecb",
+    "url": "/Akinator_Int20h/static/js/runtime-main.6f3e347b.js"
+  }
+]);
