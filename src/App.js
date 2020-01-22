851f631c070d57a981f6f855fddf78113f8ff00f
Max Panasenko <maxim.p.jsdev@gmail.com>
2020-01-22 20:19:14 +0200

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import MainScreen from './components/MainScreen/MainScreen';
 import GameScreen from './components/GameScreen/GameScreen';
 import './App.css';
